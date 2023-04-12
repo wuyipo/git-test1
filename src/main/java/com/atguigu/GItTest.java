af303b029556ad06a3503ee4d84eaad2fdb62cc1
wuyipo <17638402728@163.com>
2023-04-12 18:06:49 +0800

    third commit

--- src/main/java/com/atguigu/GItTest.java
+++ src/main/java/com/atguigu/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
+        System.out.println("Hello git3!");
     }
 }
