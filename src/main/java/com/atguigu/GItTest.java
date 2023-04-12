a0f9d79e700088b92b71e27976eaa19445e92fb7
wuyipo <17638402728@163.com>
2023-04-12 18:06:09 +0800

    second commit

--- src/main/java/com/atguigu/GItTest.java
+++ src/main/java/com/atguigu/GItTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
